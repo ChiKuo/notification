d2414669b77e473d832075b27c146556daeba3d6
chi <heysmile1210@hotmail.com>
2016-01-16 19:09:05 +0800

    Remove surplus code

--- app/src/main/java/tw/chikuo/notification/MainActivity.java
+++ app/src/main/java/tw/chikuo/notification/MainActivity.java
@@ -30,14 +30,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Send Notification
         Button sendNotificationButton = (Button) findViewById(R.id.send_notification_button);
